fe1929aef648744cc0231169e3fe9e540b0224bd
phucbienvan <phuc.bienvan@gmail.com>
2022-08-22 20:44:35 +0700

    setup

--- main.go
+++ main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, Phuc!")
+	})
+	e.GET("/users/:id", getUser)
+	e.GET("/show", show)
+	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func getUser(c echo.Context) error {
+	id := c.Param("id")
+
+	return c.String(http.StatusOK, id)
+}
+
+func show(c echo.Context) error {
+	team := c.QueryParam("team")
+	member := c.QueryParam("member")
+
+	return c.String(http.StatusOK, "team:"+team+", member:"+member)
 }
